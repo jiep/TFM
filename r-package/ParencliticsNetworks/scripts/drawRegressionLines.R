a0f32bc085bec3f9bd42fb668f91edf825a42374
José Ignacio Escribano <jiep12345@hotmail.com>
2016-04-02 15:49:48 +0200

    Modificado para añadir distintos tipos de regresiones. Por ahora sólo funciona con regresión lineal

--- r-package/ParencliticsNetworks/scripts/drawRegressionLines.R
+++ r-package/ParencliticsNetworks/scripts/drawRegressionLines.R
@@ -13,8 +13,23 @@ drawRegressionLines = function(data, target){
     for(i in 1:columnsLength){
       for(j in 1:columnsLength){
         if(i < j){
-
-          linearPlots[[i, j]] = xyplot(data[,i] ~ data[,j], data, groups = target, pch = 20, type=c("p","r"), lwd = 4, group = target, auto.key = list(columns = nlevels(target)), xlab = cols[i], ylab = cols[j])
+          
+          linearPlots[[i, j]] = xyplot(
+            data[,i]  ~ data[,j],
+            groups = target,
+            data = data,
+            panel = function(x, y, ...) {
+              panel.superpose(x, y, ...,
+                              panel.groups = function(x,y, col, col.symbol, ...) {
+                                panel.xyplot(x, y, col=col.symbol, ...)
+                                panel.abline(lm(y~x), col.line=col.symbol)
+                              }
+              )
+            },
+            auto.key = list(columns = nlevels(target)),
+            xlab = cols[i], 
+            ylab = cols[j]
+          )
         }
       }
     }
